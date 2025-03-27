14f5ce424e3ec40f8ee0a7e80471d79cb1298228
huytran298 <huytranquoc298@gmail.com>
2025-03-27 19:21:38 +0700

    just commit and still fail

--- src/file_creator/creator.js
+++ src/file_creator/creator.js
@@ -1,1 +1,26 @@
 const fs = require('fs');
+const { Crawler } = require('../parser/crawler');
+const { json } = require('stream/consumers');
+const crawler = new Crawler();
+class Creator {
+  constructor() {
+    this.name = "creator";
+  }
+  createContest() {
+    var text;
+    let ans = crawler.getProblem('codeforces', '1321', 'C').then(
+      function() {
+        
+        //return item;
+        return new Promise((resolve, reject) => {
+          resolve(ans);
+        }
+        );
+      }
+    );
+    text = ans;
+    text = JSON.parse(text);
+    return text;
+  }
+}
+module.exports = { Creator };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const { Crawler } = require('./parser/crawler');
+const { Creator } = require('./file_creator/creator');
 const { generate_test_file } = require('./file_creator/generate_testcase.js');
 const crawler = new Crawler();
+const creator = new Creator();
 
 // crawler.getProblem('codeforces', '3075', 'A')
 //   .then(item => console.log(JSON.stringify(item, null, 2)))
@@ -11,9 +13,9 @@ const crawler = new Crawler();
 //   .catch(err => console.log(err));
 const dir = "/Users/distiled/Dev/online-judge-supporter";
 
-crawler.getProblem('atcoder', 'abc398', 'abc398_d')
-  .then(item => generate_test_file(dir, item["testcase"]))
-  .catch(err => console.log(err));
+// crawler.getProblem('atcoder', 'abc398', 'abc398_d')
+//   .then(item => generate_test_file(dir, item["testcase"]))
+//   .catch(err => console.log(err));
 
-
+console.log(creator.createContest());
 
