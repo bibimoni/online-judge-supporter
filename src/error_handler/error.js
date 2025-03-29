62a93cc2d9cd822b65f3ce3a45e5de9e5050f10d
bibimoni <proquadi66@gmail.com>
2025-03-30 02:59:01 +0700

    add error_handler

---
+++ src/error_handler/error.js
@@ -0,0 +1,27 @@
+class Exception {
+  static CanNotCreateFile() {
+    return new Error("Can't not create file");
+  }
+
+  static NoSourceFile(fileName) {
+    return new Error(`No source file with name: ${fileName}`);
+  }
+
+  static NoTestFileAvailable(fileName) {
+    return new Error(`No test file for source file: ${fileName}`);
+  }
+
+  static FileNotFound(fileName) {
+    return new Error(`File Not Found ${fileName}`)
+  }
+  
+  static TestFileNotFound(fileName, fileIndex) {
+    return new Error(`Test number ${fileIndex} is not available for source file: ${fileName}`)
+  }
+
+  static NoBinaryFile(fileName) {
+    return new Error(`No binary file found for source file: ${fileName}`);
+  }
+}
+
+module.exports = { Exception };
