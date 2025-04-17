d93dadb1de4b7d39c08a69010f9e85cf44fdbf54
bibimoni <proquadi66@gmail.com>
2025-04-17 11:42:03 +0700

    fix testcase

--- src/commands/sub_commands/test.js
+++ src/commands/sub_commands/test.js
@@ -2,19 +2,36 @@ const { Logger } = require('../logger');
 const { Compiler } = require('../../compiler/compiler');
 const path = process.cwd();
 const { TestCase } = require('../../test/testcase');
+const { Exception } = require('../../error_handler/error');
+const { Verdict } = require('../../test/verdict');
+
 const testCommand = (program) =>  {
   program
     .command('test')
     .description('Test source file with testcases')
     .alias('t')
     .argument('<filename>', 'Name of the source code (a.rs, b.cpp, c.py, ...)') 
-    .action(fileName => {
+    .option('-t, --test <index>', 'test index (starts from 1)')
+    .action((fileName, options) => {
       const filePath = `${path}/${fileName}`;
+      const testIndex = options.test;
+      let testEntry = undefined;
+      if (testIndex) {
+        testEntry = (parseInt(testIndex) - 1).toString();
+      }
       try {
         const compiler = new Compiler({ filePath: filePath });
         compiler.buildFile();
-      } catch (e) {
-        Logger.logErrorSpinner(e.message);
+        const testcases = TestCase.getTestCasesFromDirectory(filePath);
+        if (Object.keys(testcases).length === 0) {
+          Logger.logErrorSpinner(Exception.noTestAvailable(fileName).message);
+          return;
+        } if (testEntry && !testcases[testEntry]) {
+          Logger.logErrorSpinner(Exception.testFileNotFound(fileName, testIndex).message);
+          return;
+        }
+      } catch (_) {
+        Logger.logVerdict(Verdict.CE);
       }
     });
 };
--- src/test/verdict.js
+++ src/test/verdict.js
@@ -3,15 +3,17 @@ class Verdict {
   static #_TLE = 1;
   static #_RE = 2;
   static #_WA = 3;
-  static #_COMPLILE_ERROR = 5;
+  static #_CE = 5;
   static #_UNKNOWN = 4;
+  static #_AC = 6;
 
   static get MLE() { return this.#_MLE; }
   static get TLE() { return this.#_TLE; }
   static get RE() { return this.#_RE; }
   static get WA() { return this.#_WA; }
-  static get COMPLILE_ERROR() { return this.#_COMPLILE_ERROR; }
+  static get CE() { return this.#_CE; }
   static get UNKNOWN() { return this.#_UNKNOWN; }
+  static get AC() { return this.#_AC; }
 }
 
 module.exports = { Verdict };
